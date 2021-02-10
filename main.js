254b5604b47931c45d860cacec102efc014a1150
Ricardo Martinez Osorio <ricardo.martinez@colfive.com>
2021-02-10 14:37:33 -0500

    add push

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+"use strict";
+
+const applicationServerPublicKey =
+  "BHENLuD--woX1DilPIVzIwmBMpI47xih8jrej2_yGWo3B5eF-TlcYncH9omVkOJyPF2yT4CTUIwOgZ8AJQ_tiSM";
+
+function urlB64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 let deferredPrompt;
 
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && "PushManager" in window) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/sw.js")
@@ -26,8 +46,27 @@ window.addEventListener("beforeinstallprompt", (e) => {
     }
     hideInstallPromo();
   });
+  deferredPrompt = null;
   showInstallPromo();
 });
+
+Notification.requestPermission((status) => {
+  console.log("Notification permission status", status);
+});
+
+function displayNotification() {
+  if (Notification.permission === "granted") {
+    navigator.serviceWorker.getRegistration().then((reg) => {
+      const options = {
+        body: "Here is a notification body",
+        icon: "icon-512.png",
+        vibrate: [100, 50, 100],
+        data: { primaryKey: 1 },
+      };
+      reg.showNotification("hello world", options);
+    });
+  }
+}
 
 // setTimeout(() => showInstallPromo(), 3000);
 
